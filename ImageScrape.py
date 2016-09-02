a408dab1a853d6e45e1aa6100136e804cd7b4298
Victor Chen <vyc@victorchen.org>
2016-09-01 22:10:02 -0700

    adds comments for saving to a platform independent directory path

--- ImageScrape.py
+++ ImageScrape.py
@@ -25,6 +25,13 @@ url="https://www.google.com/search?q="+query+"&source=lnms&tbm=isch"
 print(url)
 
 #add the directory for your image here
+"""
+# TODO: Consider defining a platform independent DIR.
+delimiter = ('/', '//')[os.name == "nt"]
+DIR = delimiter.join([os.path.expanduser('~'), "Downloads", "image_dw",
+    (query.split('+'))[0], ""])
+"""
+
 DIR="//Users//Downloads//image_dw"+(query.split('+'))[0]+"//"
 print(DIR)
 header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
@@ -48,12 +55,11 @@ for i , (img , Type) in enumerate( ActualImages):
             os.mkdir(DIR)
         cntr = len([i for i in os.listdir(DIR) if image_type in i]) + 1
         print ("imgNumber:", cntr)
-        if len(Type)==0:
-            f = open(DIR + image_type + "_"+ str(cntr)+".jpg", 'wb')
-        else :
-            f = open(DIR + image_type + "_"+ str(cntr)+"."+Type, 'wb')
- 
- 
+        Type = (Type, "jpg")[len(Type) == 0]
+
+        filepath = DIR + image_type + "_" + str(cntr) + "." + Type
+        print("Saving to filepath: ", filepath)
+        f = open(filepath, 'wb')
         f.write(raw_img)
         f.close()
     except Exception as e:
